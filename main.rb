aaabf6080d36f69f8e447038d3d150d9d26fd741
kouji0224 <k.kouji0224@gmail.com>
2020-02-03 11:39:06 +0900

    見た目とRSS追加

--- main.rb
+++ main.rb
@@ -6,6 +6,9 @@ get '/' do
     @tokyoshinbun = RSS::Parser.parse("http://www.zou3.net/php/rss/tokyo2rss.php?cat=main")
     @cnn = RSS::Parser.parse("http://feeds.cnn.co.jp/cnn/rss")
     @gihyo = RSS::Parser.parse("https://gihyo.jp/feed/rss2")
-    @array = [@hatena, @tokyoshinbun, @cnn, @gihyo]
+    @wired = RSS::Parser.parse("https://wired.jp/rssfeeder/")
+    @buzzfeed = RSS::Parser.parse("https://www.buzzfeed.com/jp/index.xml?country=ja-jp")
+    @afp = RSS::Parser.parse("http://feeds.afpbb.com/rss/afpbb/afpbbnews")
+    @array = [@hatena, @tokyoshinbun, @cnn, @gihyo, @wired, @buzzfeed, @afp]
     erb :index
 end
